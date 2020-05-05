be8a60f5b21e3f9d337f352a157aab6dcd086f9f
Mark Andrews <mjandrews.org@gmail.com>
2020-05-05 20:17:22 +0100

    Add the analysisproject package

---
+++ notebooks/analysisproject/analysis.py
@@ -0,0 +1,29 @@
+'''
+This modules creates some functions.
+
+This code is free and open source software. GPL 3.0.
+Copyright by Mark Andrews, 2020.
+
+'''
+
+x = 42
+y = 'foobar'
+z = 'mark'
+
+def power(x, k):
+
+    '''
+    This function raises a number to the power
+    of k.
+    '''
+
+    return x ** k
+
+def sqrt(x):
+
+    '''
+    Return square root of x.
+
+    '''
+
+    return x ** 0.5
